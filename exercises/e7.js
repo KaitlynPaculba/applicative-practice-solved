13820ebbe4fbc976ae071048e177839651b2b0fa
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-30 14:00:06 -0900

    e7.js Solved

--- exercises/e7.js
+++ exercises/e7.js
@@ -1,11 +1,27 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 7
 // Return an array of all Planets names that have moons
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsNamesWithMoons(data) {
-  // Your code goes here...
+  const planets = data.planets.map((obj) => obj.name);
+const planMoon = data.planets.map((obj) => obj.moons);
+const planLength = planets.length;
+const planMoonArr = [];
+
+function objConst() {
+  let a = 0;
+  while (a < planLength) {
+    const newObj = { name: planets[a], moons: planMoon[a] };
+    planMoonArr.push(newObj);
+    a = a + 1;
+  }
+};
+objConst();
+const moonFilt = planMoonArr.filter((obj) => obj.moons != undefined);
+const planMoonTrue = moonFilt.map((obj) => obj.name);
+  return planMoonTrue;
 }
 
 
