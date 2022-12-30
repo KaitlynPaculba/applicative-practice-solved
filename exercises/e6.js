1f24656e9684a90327596a8b7f5cdfd84b3f8b60
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-30 13:49:17 -0900

    e6.js solved

--- exercises/e6.js
+++ exercises/e6.js
@@ -1,13 +1,29 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 6
 // Return an array with all asteroids names discovered after a given year
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getAsteroidsDiscoveredAfterYear(data, year) {
-  // Your code goes here...
-}
+const asteroids = data.asteroids.map((obj) => obj.name)
+const astYear = data.asteroids.map((obj) => obj.discoveryYear);
+const astYearCon = [];
+const astlength = asteroids.length;
 
+function objConst() {
+  let a = 0;
+  while (a < astlength) {
+    let newObj = { name: asteroids[a], discoveryYear: astYear[a] };
+    astYearCon.push(newObj);
+    a = a + 1;
+  }
+};
+objConst();
+const yearFilt = astYearCon.filter((obj) => obj.discoveryYear > year);
+const finalDiscYearArr = yearFilt.map((obj) => obj.name);
+  year = year + 1;
+return finalDiscYearArr;
+}
 
 
 // === TEST YOURSELF ===
