29851fcfe61c32f715fdaea6000ec3aeebab0451
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-30 14:27:12 -0900

    e8.js Solved

--- exercises/e8.js
+++ exercises/e8.js
@@ -1,12 +1,37 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 8
 // Return a Planet name by a given moon name
 // Return example: 'Planet Name'
 
-export function findPlanetNameByMoon(data, moonName) {
-  // Your code goes here...
-}
+ export function findPlanetNameByMoon(data, moonName) {
+  const planets = data.planets.map((obj) => obj.name);
+  const planMoon = data.planets.map((obj) => obj.moons);
+  const planLength = planets.length;
+  const planMoonArr = [];
+  function objConst() {
+    let a = 0;
+    while (a < planLength) {
+      const newObj = { name: planets[a], moons: planMoon[a] };
+      planMoonArr.push(newObj);
+      a = a + 1;
+    }
+  };
+  objConst();
+  function MoonFilt() {
+    let a = 0;
+    while (a < planLength) {
+      if (planMoonArr[a].moons.includes(moonName)) {
+        return planMoonArr[a].name;
+      } else {
+        a = a + 1
+      }
+    }
+  };
+  MoonFilt();
+  const MoonFiltTag = MoonFilt();
+   return MoonFiltTag;
+ }
 
 
 
