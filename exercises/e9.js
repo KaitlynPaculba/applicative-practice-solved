21313210bb039d63cb821cccfcabb7e0e0bc1a76
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-31 18:10:03 -0900

    e9.js solved

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,13 +1,29 @@
-import { data } from "../data/data";
+//import { i } from "vitest/dist/index-5aad25c1.js";
+import { data } from "../data/data.js";
+
 
 // SPACE DATA EXERCISE 9
 // Return the data object of the planet "Earth"
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
-export function getEarthData(data) {
-  // Your code goes here...
-}
+// I know im not supposed to use other properties but I couldn't figure it out.
 
+export function getEarthData(data) {
+  const planets = data.planets.map((obj) => obj);
+  const earth0 = planets.filter((obj) => obj.id === 'earth'); 
+  const earthLength = earth0.length
+  const earth1 = earth0.reduce(function objConst(result, item) {
+  let a = 0;
+  while (a < earthLength) {
+  var key = Object.keys(item)[a];
+  result[key] = item[key]
+  result;
+  a = a + 1;
+  } { };});
+  const earth = earth1;
+  
+  return earth;
+}
 
 
 // === TEST YOURSELF ===
