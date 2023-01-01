ced51676dc8b28b77dffb37ff7322f37c0049050
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-31 18:27:34 -0900

    e10.js Solved

--- exercises/e10.js
+++ exercises/e10.js
@@ -1,11 +1,23 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 10
 // Return a given asteroid object of data
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getAsteroidDataByName(data, asteroidName) {
-  // Your code goes here...
+const asteroids = data.asteroids.map((obj) => obj);
+const asteroidSearch = asteroids.filter((obj) => obj.name === asteroidName)
+const asteroidLength = asteroidSearch.length;
+const asteroid = asteroidSearch.reduce(function objConst(result, item) {
+  let a = 0;
+  while (a < asteroidLength) {
+  var key = Object.keys(item)[a];
+  result[key] = item[key]
+  result;
+  a = a + 1;
+  } { };});
+  const selectAsteroid = asteroid;
+  return selectAsteroid;
 }
 
 
