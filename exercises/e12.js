8949d5ff50dd54b298ec08eed28ee5552ae96629
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-31 19:19:50 -0900

    e12.js Solved

--- exercises/e12.js
+++ exercises/e12.js
@@ -1,11 +1,29 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 12
 // Return the sum of all moons for all planets
 // Return example: 42
 
+
+
 export function allPlanetsMoonsCount(data) {
-  // Your code goes here...
+const planets = data.planets.map((obj) => obj);
+const moonCount = planets.map((obj) => obj.moonsCount);
+const moonNum = moonCount.filter((obj) => obj > 0);
+const moonLength = moonNum.length;
+const equationAdd = [];
+const test = moonNum.values();
+function moonNumEquate() {
+  let a = 0;
+  while (a < moonLength) {
+  const newObj =  moonNum[a] + moonNum[a + 1];
+    equationAdd.push(newObj);
+    a = a + 2;
+  }
+};
+moonNumEquate();
+let sum = equationAdd[0] + equationAdd[1] + equationAdd[2];
+  return sum;
 }
 
 
