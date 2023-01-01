06240286ad63a323b026dc8090634756460c842e
Kaitlyn Paculba <kaitlyn.paculba03@gmail.com>
2022-12-31 19:05:40 -0900

    e11.js Solved

--- exercises/e11.js
+++ exercises/e11.js
@@ -1,11 +1,34 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 11
 // Return an array of Planets' names with less than 10 moons
 // Return example: ['name1', 'name2', ... , 'nameN']
 
+
+
+
 export function lowMoonsPlanets(data) {
-  // Your code goes here...
+ const planets = data.planets.map((obj) => obj);
+const moons = planets.moons;
+const pLength = planets.length;
+const lowMoons = [];
+const notLow = [];
+function MoonFilt() {
+  let a = 0;
+  while (a < pLength) {
+    if (planets[a].moonsCount < 10) {
+      lowMoons.push(planets[a]);
+    } else {
+      notLow.push(planets[a]);
+    }
+    a = a + 1
+  }
+};
+MoonFilt();
+const lowMPlanNames0 = lowMoons.filter((obj) => obj.name);
+const lowMPlanNames = lowMPlanNames0.map((obj) => obj.name);
+
+  return lowMPlanNames;
 }
 
 
